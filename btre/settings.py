34d2f985203351e70545bd0762ee1a99e55b7242
John Ripplinger <51840420+JohnRipplinger@users.noreply.github.com>
2020-06-23 20:50:36 -0500

    added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -150,3 +150,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'site_admin@comcast.net'
 EMAIL_HOST_PASSWORD = 'mypassword'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
